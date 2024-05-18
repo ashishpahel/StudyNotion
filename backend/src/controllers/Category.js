394652509038422ebf4485b8a128694a126d1427
ashishpahel <ashishpahel1999@gmail.com>
2024-05-18 18:50:12 +0530

    category controller complete

---
+++ backend/src/controllers/Category.js
@@ -0,0 +1,99 @@
+const Category = require("../models/Category");
+
+exports.createCategory = async (req, res) => {
+    try {
+        const { name, description } = req.body;
+
+        if (!name || !description) {
+            return res
+                .status(400)
+                .json({ success: false, message: "All fields are required" });
+        }
+
+        Category.create({
+            name: name,
+            description: description,
+        });
+
+        console.log(CategorysDetails);
+
+        return res.status(200).json({
+            success: true,
+            message: "Categorys Created Successfully",
+        });
+
+    } catch (error) {
+        return res.status(500).json({
+            success: true,
+            message: "Category not created",
+            error: error.message
+        });
+    }
+}
+
+exports.showAllCategories = async (req, res) => {
+    try {
+        const allCategorys = await Category.find(
+            {},
+            { name: true, description: true }
+        );
+        res.status(200).json({
+            success: true,
+            data: allCategorys,
+        });
+    } catch (error) {
+        return res.status(500).json({
+            success: false,
+            message: "Data not found",
+            error: error.message,
+        });
+    }
+};
+
+//categoryPageDetails 
+
+exports.categoryPageDetails = async (req, res) => {
+    try {
+        //get categoryId
+        const { categoryId } = req.body;
+
+        //get courses for specified categoryId
+        const selectedCategory = await Category.findById(categoryId)
+            .populate("courses")
+            .exec();
+
+        if (!selectedCategory) {
+            return res.status(404).json({
+                success: false,
+                message: 'Data Not Found',
+            });
+        }
+
+        //get coursesfor different categories
+        const differentCategories = await Category.find({
+            _id: { $ne: categoryId },
+        })
+            .populate("courses")
+            .exec();
+
+
+
+        //return response
+        return res.status(200).json({
+            success: true,
+            data: {
+                selectedCategory,
+                differentCategories,
+            },
+        });
+
+    }
+    catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            success: false,
+            message: "Something Went Wrong",
+            error: error.message
+        });
+    }
+}
