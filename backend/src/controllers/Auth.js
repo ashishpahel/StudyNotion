b78cdd56a92cb5a4178087b9ef7ae322b29badce
ashishpahel <ashishpahel1999@gmail.com>
2024-05-18 17:43:52 +0530

    login controller complete

--- backend/src/controllers/Auth.js
+++ backend/src/controllers/Auth.js
@@ -10,7 +10,7 @@ require("dotenv").config();
 
 // Signup Controller for Registering USers
 
-exports.signup = async (req, res) => { 
+exports.signup = async (req, res) => {
     try {
         // Destructure fields from the request body
         const {
@@ -104,6 +104,79 @@ exports.signup = async (req, res) => {
         return res.status(500).json({
             success: false,
             message: "User cannot be registered. Please try again.",
+        });
+    }
+}
+
+// Login controller for authenticating users
+
+exports.login = async (req, res) => {
+    try {
+        // Get email and password from request body
+        const { email, password } = req.body;
+
+        // Check if email or password is missing
+        if (!email || !password) {
+            return res.status(400).json({
+                success: false,
+                message: `Please Fill up All the Required Fields`,
+            });
+        }
+
+        // Find user with provided email 
+        const user = await User.findOne({ email }).populate("additionalDetails")
+
+        // If user not found with provided email
+        if (!user) {
+            return res.status(400).json({
+                success: false,
+                message: `User is not Registered with Us Please SignUp to Continue`,
+            });
+        }
+
+        // Generate JWT token and Compare Password
+        if (await bcrypt.compare(password, user.password)) {
+            const payload = {
+                id: user._id,
+                email: user.email,
+                accountType: user.accountType,
+            };
+
+            const options = {
+                expiresIn: '24h',
+            };
+
+            const secret = process.env.JWT_SECRET;
+
+            const token = jwt.sign(payload, secret, options);
+
+            // Update user document with the token
+            user.token = token;
+            await user.save();
+
+            // Set cookie for token and return success response
+
+            const cookieOptions = {
+                expires: new Date(Date.now() + 3 * 24 * 60 * 60 * 1000),
+                httpOnly: true,
+            };
+
+            res.cookie("token", token, cookieOptions).status(200).json({
+                success: true,
+                token,
+                user,
+                message: "User Login Success"
+            })
+        } else {
+            return res.status(400).json({
+                success: false,
+                message: `Password is incorrect`,
+            });
+        }
+    } catch (error) {
+        return res.status(400).json({
+            success: false,
+            message: `Login Failure Please Try Again`,
         });
     }
 }
