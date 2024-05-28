087bdfa5cca2e73ca19bc0eaafbc3fc20c6b8632
ashishpahel <ashishpahel1999@gmail.com>
2024-05-28 21:49:58 +0530

    update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,20 +1,49 @@
 const express = require("express");
-const dotenv = require("dotenv");
-const db = require('./config/database')
 const app = express();
+
+const userRoutes = require("./routes/User");
+const profileRoutes = require("./routes/Profile");
+const paymentRoutes = require("./routes/Payments");
+const courseRoutes = require("./routes/Course");
+
+const db = require('./config/database')
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const { cloudinaryConnect } = require("./config/cloudinary");
+const fileUpload = require("express-fileupload");
+const dotenv = require("dotenv");
 
 dotenv.config();
 
-
 const PORT = process.env.PORT || 4000;
 
-
+// Database connect
 db()
-.then(() => {
-    app.listen(PORT, () => {
-        console.log(`Server is running at port : ${PORT}`);
+    .then(() => {
+        app.listen(PORT, () => {
+            console.log(`Server is running at port : ${PORT}`);
+        })
     })
-})
-.catch((err) => {
-    console.log("MONGO db connection failed !!! ", err);
-})
+    .catch((err) => {
+        console.log("MONGO db connection failed !!! ", err);
+    })
+
+// Middleware setup
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors({ origin: FRONTEND_URL, credentials: true }));
+app.use(fileUpload({ useTempFiles: true, tempFileDir: "/tmp" }));
+
+//cloudinary connection
+cloudinaryConnect();
+
+// Define routes
+app.use("/api/v1/auth", userRoutes);
+app.use("/api/v1/profile", profileRoutes);
+app.use("/api/v1/course", courseRoutes);
+app.use("/api/v1/payment", paymentRoutes);
+
+// Default route
+app.get("/", (req, res) => {
+    res.json({ success: true, message: 'Server is up and running...' });
+});
