63804ef488e64831231ab15f7845e4c1aa3f8c57
ashishpahel <ashishpahel1999@gmail.com>
2024-06-02 12:22:12 +0530

    home page and nav bar complete

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import Home from './pages/Home'
 import Footer from './components/common/Footer'
+import Navbar from './components/common/Navbar'
+
 
 function App() {
   return (
-    <div><Footer/></div>
+    <div className='flex min-h-screen w-screen flex-col bg-richblack-900 font-inter'>
+
+      <Navbar />
+      <Home/>
+    </div>
   )
 }
 
